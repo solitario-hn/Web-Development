2774bf20bc631f029082a88cd48ca968c8a5d4f4
solitario <elsolitariolafe@gmail.com>
2025-11-08 19:14:13 +0000

    ko

--- selenium/del.py
+++ selenium/del.py
@@ -1,3 +1,8 @@
-am="Cursor - 15"
-amt=am.lstrip(' - ')
+am="Time machine - 123,456,789"
+amt=am.split(' - ')[1].strip().replace(',',"")
+key=amt.strip(',')
 print(amt)
+print(key)
+
+
+#element_text.split("-")[1].strip().replace(",", "")
